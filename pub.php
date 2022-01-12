cf5a48c605d329bd5515126bbb9af57021dcc808
cutter1020 <58689319+cutter1020@users.noreply.github.com>
2022-01-12 15:35:49 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -25,30 +25,26 @@ function pubMqtt($topic,$msg){
 
 	$mqtt->connect($connectionSettings, $clean_session);
 	//printf("client connected\n");
-
+/*
 	$mqtt->subscribe('test555', function ($topic, $message) {
     	printf("Received message on topic [%s]: %s\n", $topic, $message);
 	}, 0);
-
-	for ($i = 0; $i< 10; $i++) {
-  		$payload = array(
-    		'protocol' => 'tcp',
-    		'date' => date('Y-m-d H:i:s'),
-    		'url' => 'https://github.com/emqx/MQTT-Client-Examples'
-  		);
-  		$mqtt->publish(
-    		// topic
-    		'test555',
-    		// payload
-    		json_encode($payload),
-    		// qos
-    		0,
-    		// retain
-    		true
-  		);
-  		//printf("msg $i send\n");
-  		sleep(1);
- 	}
+*/
+  	$payload = array(
+    	'message' => $msg,
+  	);
+  	$mqtt->publish(
+    	// topic
+    	$topic,
+    	// payload
+    	json_encode($payload),
+    	// qos
+    	0,
+    	// retain
+    	true
+  	);
+  	//printf("msg $i send\n");
+  	sleep(1);
 }
 
 function getMqttfromlineMsg($Topic,$lineMsg){
